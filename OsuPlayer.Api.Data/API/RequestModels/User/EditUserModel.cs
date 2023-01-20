a3e6a9c5e27c57ea83dee644ff00cad3e3edd5a5
Founntain <7@founntain.dev>
2023-01-20 12:22:54 +0100

    added newusername to edit model

--- OsuPlayer.Api.Data/API/RequestModels/User/EditUserModel.cs
+++ OsuPlayer.Api.Data/API/RequestModels/User/EditUserModel.cs
@@ -5,5 +5,6 @@ namespace OsuPlayer.Api.Data.API.RequestModels.User;
 public class EditUserModel
 {
     public UserModel User { get; set; }
+    public string? NewUsername { get; set; }
     public string? NewPassword { get; set; }
 }
