33daaecaa7fd304920db8ef2157a98ec22604e79
Founntain <7@founntain.dev>
2023-05-25 18:58:37 +0200

    changed party models

--- OsuPlayer.Api.Data/API/EntityModels/PartyModel.cs
+++ OsuPlayer.Api.Data/API/EntityModels/PartyModel.cs
@@ -8,6 +8,12 @@ public class PartyModel : BaseModel
     public bool IsPaused { get; set; }
     public bool IsPrivate { get; set; }
     
+    // Data we need for displaying purpose and if the beatmap doesn't exist on the api
+    public string BeatmapHash { get; set; }
+    public string FullSongname { get; set; }
+    
+    // In case we need it in the future
     public BeatmapModel Beatmap { get; set; }
+    
     public HashSet<UserModel> UsersInParty { get; set; }
 }
--- OsuPlayer.Api.Data/API/RequestModels/Party/CreatePartyModel.cs
+++ OsuPlayer.Api.Data/API/RequestModels/Party/CreatePartyModel.cs
@@ -3,9 +3,12 @@
 public class CreatePartyModel
 {
     public double PlaybackSpeed { get; set; }
+    public bool UsePitch { get; set; }
     public double Timestamp { get; set; }
     public bool IsPaused { get; set; }
     
-    public Guid? HostId { get; set; }
     public Guid? BeatmapId { get; set; }
+    
+    public string BeatmapHash { get; set; }
+    public string FullSongname { get; set; }
 }
