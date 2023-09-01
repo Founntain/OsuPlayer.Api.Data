db63f1210cbc41236ff497860ba50f3a5387047b
Founntain <7@founntain.dev>
2023-09-01 12:20:17 +0200

    Added page and size

--- OsuPlayer.Api.Data/API/RequestModels/Beatmap/SearchBeatmapModel.cs
+++ OsuPlayer.Api.Data/API/RequestModels/Beatmap/SearchBeatmapModel.cs
@@ -2,6 +2,8 @@
 
 public class SearchBeatmapModel
 {
+    public int Page { get; set; } = 1;
+    public int PageSize { get; set; } = 64;
     public string? Artist { get; set; }
     public string? Title { get; set; }
     public string? BeatmapSetId { get; set; }
