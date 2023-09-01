bf1f6a28b8993a63908b959749a95d9bfd0a4b5b
Founntain <7@founntain.dev>
2023-09-01 11:39:16 +0200

    Added SearchBeatmapModel

---
+++ OsuPlayer.Api.Data/API/RequestModels/Beatmap/SearchBeatmapModel.cs
@@ -0,0 +1,10 @@
+﻿using OsuPlayer.Api.Data.API.EntityModels;
+
+namespace OsuPlayer.Api.Data.API.RequestModels.Beatmap;
+
+public class SearchBeatmapModel
+{
+    public int Page { get; set; }
+    public int PageSize { get; set; }
+    public Func<BeatmapModel, bool>? Filter { get; set; } = null;
+}
