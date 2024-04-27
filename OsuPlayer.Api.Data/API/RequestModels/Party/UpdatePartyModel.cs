b7c48b52f10a781bf328bff61bd976d95775315e
Founntain <7@founntain.dev>
2024-04-27 19:53:14 +0200

    Add beatmap search parameters to UpdatePartyModel

--- OsuPlayer.Api.Data/API/RequestModels/Party/UpdatePartyModel.cs
+++ OsuPlayer.Api.Data/API/RequestModels/Party/UpdatePartyModel.cs
@@ -4,8 +4,12 @@ public class UpdatePartyModel
 {
     public Guid? UserId { get; set; }
     public Guid? PartyId { get; set; }
-    
+
     public double PlaybackSpeed { get; set; }
     public bool IsPaused { get; set; }
     public bool IsPrivate { get; set; }
+
+    // Fallback values and for beatmap search
+    public string BeatmapHash { get; set; }
+    public string FullsongName { get; set; }
 }
